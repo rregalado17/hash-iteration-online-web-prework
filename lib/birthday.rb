14660c618cd94c9d7df2b83c957ca26c9aecada9
magical-flow-3186 <rregalado17@gmail.com>
2019-06-06 22:24:34 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -5,13 +5,10 @@
 # }
 
 
+
 def happy_birthday(birthday_kids)
   birthday_kids.each do |kids_name, age|
-
     puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
-  else
-    puts "You are too old for this."
-  end
   end
 end
 
